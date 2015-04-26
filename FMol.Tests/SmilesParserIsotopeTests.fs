d7aaf402b78f6f1350333210b3639e358607a3fc
Ian Reah <ian.reah@gmail.com>
2015-04-26 12:36:58 +0100

    Generators for testing an isotope parser
    The simplest 'primitive' of the SMILES parser is going to be the isotope parser. It's basically
    just a positive natural number within an acceptable range...
    
    Excerpt from the OpenSMILES specification:
    
    > An isotope is interpreted as a number, so that [2H], [02H] and [002H] all mean deuterium.
    ...A general-purpose SMILES parser must accept at least three digits for the isotope and values
    from 0 to 999
    
    `validIsotopeGenerator` randomly generates valid isotope strings (with the expected isotope value)
    according to the above specification (i.e., within the specified range and with 0 or more leading
    zeros), e.g.,...
    
    > Gen.sample 10 3 validIsotopeGenerator;;
    val it : (string * uint32) list =
      [("00093", 93u); ("00379", 379u); ("000000626", 626u)]
    
    `outOfRangeIsotopeGenerator` generates strings that could be valid isotopes except that they are
    outside of the acceptable range, e.g.,...
    
    > Gen.sample 10 3 outOfRangeIsotopeGenerator;;
    val it : string list = ["-4"; "1005"; "-7"]
    
    `invalidIsotopeGenerator` generates strings that are nothing like valid isotopes, e.g.,...
    
    > Gen.sample 10 3 invalidIsotopeGenerator;;
    val it : string list = ["Rtj6{"; "g}0;0&"; "ig"]
    
    It should now be fairly trivial to use these generators and the helper functions from the previous
    commit to create some fscheck properties to test an isotope parser.

---
+++ FMol.Tests/SmilesParserIsotopeTests.fs
@@ -0,0 +1,29 @@
+﻿module FMol.Tests.SmilesParserIsotopeTests
+
+open System
+open FsCheck
+
+let validIsotopeGenerator = gen {
+    // A general-purpose SMILES parser must accept at least three digits
+    // for the isotope and values from 0 to 999
+    let! validIsotope = Gen.choose (0, 999)
+
+    // An isotope is interpreted as a number, so that [2H], [02H] and
+    // [002H] all mean deuterium.
+    let! padWithZeros = Arb.generate<bool>
+    let! zeroCount = if padWithZeros then Arb.generate<uint32> else (Gen.constant 0u)
+    let parserInput = String.Format("{0}{1}", (String.replicate (int zeroCount) "0"), validIsotope)
+
+    return parserInput, (uint32 validIsotope);
+}
+
+let outOfRangeIsotopeGenerator = gen {
+    let! neg = Arb.generate<bool>
+    let! invalidIsotope = Gen.suchThat (fun x -> if neg then x < 0 else x > 999) Arb.generate<int>
+    return invalidIsotope.ToString()
+} 
+
+let invalidIsotopeGenerator = gen {
+    let! s = Gen.suchThat (fun x -> x <> Unchecked.defaultof<string>) Arb.generate<string>
+    return s.TrimStart([| '0'..'9' |])
+}
