90ec406dd371a6248d9e35ccd9724b00a7b4d310
Ian Reah <ian.reah@gmail.com>
2015-05-01 19:51:21 +0100

    Generators for testing element symbols parser
    This is the list of element symbols from the OpenSMILES specification. It looks like it's
    just the 114 confirmed elements (without "ununtrium" etc).
    
    `elementSymbolGenerator` will randomly choose any one of the symbols, and
    `notAnElementSymbolGenerator` generates random strings which are guaranteed to
    not start with an element symbol.
    
    Note `elementSymbolGenerator` returns the `symbol` twice in a tuple so it can be
    used with `testParserSucceedsWith` which expects a parser and a 2-element tuple
    where the first element is the string to be parsed and the second element is the
    expected result from the parser. In this case, the parser is expected to simply
    return the symbol.

---
+++ FMol.Tests/SmilesParserElementSymbolTests.fs
@@ -0,0 +1,27 @@
+﻿module FMol.Tests.SmilesParserElementSymbolTests
+
+open FsCheck
+
+let elementSymbols =
+    [ "H"; "He"; "Li"; "Be";  "B";  "C";  "N";  "O";  "F"; "Ne"; "Na"; "Mg";
+     "Al"; "Si";  "P";  "S"; "Cl"; "Ar";  "K"; "Ca"; "Sc"; "Ti";  "V"; "Cr";
+     "Mn"; "Fe"; "Co"; "Ni"; "Cu"; "Zn"; "Ga"; "Ge"; "As"; "Se"; "Br"; "Kr";
+     "Rb"; "Sr";  "Y"; "Zr"; "Nb"; "Mo"; "Tc"; "Ru"; "Rh"; "Pd"; "Ag"; "Cd";
+     "In"; "Sn"; "Sb"; "Te";  "I"; "Xe"; "Cs"; "Ba"; "Hf"; "Ta";  "W"; "Re";
+     "Os"; "Ir"; "Pt"; "Au"; "Hg"; "Tl"; "Pb"; "Bi"; "Po"; "At"; "Rn"; "Fr";
+     "Ra"; "Rf"; "Db"; "Sg"; "Bh"; "Hs"; "Mt"; "Ds"; "Rg"; "Cn"; "Fl"; "Lv";
+     "La"; "Ce"; "Pr"; "Nd"; "Pm"; "Sm"; "Eu"; "Gd"; "Tb"; "Dy"; "Ho"; "Er";
+     "Tm"; "Yb"; "Lu"; "Ac"; "Th"; "Pa";  "U"; "Np"; "Pu"; "Am"; "Cm"; "Bk";
+     "Cf"; "Es"; "Fm"; "Md"; "No"; "Lr"]
+
+let elementSymbolGenerator = gen {
+    let! i = Gen.choose (0, List.length elementSymbols-1)
+    let symbol = (List.nth elementSymbols i)
+    return (symbol, symbol)
+}
+
+let notAnElementSymbolGenerator =
+    let startsWithAnElementSymbol (s:string) =
+        elementSymbols |> List.exists (fun x -> s.StartsWith x)
+    Gen.suchThat (fun x -> x <> Unchecked.defaultof<string> && not (x |> startsWithAnElementSymbol)) Arb.generate<string>
+
